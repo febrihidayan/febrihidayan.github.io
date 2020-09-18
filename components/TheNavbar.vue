38ac020b22a163f7dd2c98e5cc7348a64237c6bd
Febri Hidayan <febrihidayan20@gmail.com>
2020-09-19 00:20:17 +0700

    menambah navbar

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -1,3 +1,19 @@
 <template>
-
+    <nav class="navbar">
+        <div class="container">
+            <div class="navbar-brand">
+                <NuxtLink to="/" class="navbar-item">Febri Hidayan</NuxtLink>
+                <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+            <div class="navbar-menu" id="navbarBasicExample">
+                <div class="navbar-start">
+                    <NuxtLink to="/">Blog</NuxtLink>
+                </div>
+            </div>
+        </div>
+    </nav>
 </template>
